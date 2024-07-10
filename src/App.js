e305161e12b748abb0dd85df8b1d3eaeda31e91e
Sissoko Modibo <m.sissoko.7035@gmail.com>
2024-07-10 17:00:56 +0000

    Initialisation de l'appli Meteo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+
 import './App.css';
+import { useStateValue } from './store';
+import Notification from './Notification';
+import WeatherInfo from './WeatherInfo';
+
+import CityInfo from './City';
+import { getWeather, KEY, updateWeather } from './reducers';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [{error}, dispatch] = useStateValue();  
+
+  useEffect(() => {     
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {  
+        let payload = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        };
+        dispatch({
+          type: "SET_LOCATION",
+          payload
+        });
+        let api = `http://api.openweathermap.org/data/2.5/weather?lat=${payload.latitude}&lon=${payload.longitude}&appid=${KEY}`;
+        getWeather(api)
+        .then((data) => {
+          updateWeather(dispatch, data);          
+        })
+        .catch (e => {            
+            dispatch({
+              type: "SET_ERROR",
+              payload: {
+                error: e
+              }
+            });
+        });                   
+      }, (error) => {  
+        dispatch({
+          type: "SET_ERROR",
+          payload: {
+            error
+          }
+        });              
+      });
+    }
+  }, []);  
+
+  return (    
+    <div className="app">      
+      <CityInfo />
+      {error && <Notification />}
+      {!error && <WeatherInfo />}
     </div>
   );
 }
